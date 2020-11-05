3cbe81532842055cf24b7d7d1bb9ffa5a332f84c
patriciosoto <72085773+patriciosoto@users.noreply.github.com>
2020-11-05 10:46:15 -0300

    Quinto Commit
    Le agregamos mas codigo en el AResultado y modificamos los colores y le colocamos el boton

--- app/src/main/java/cl/ist/proyectoapp3/AResultado.java
+++ app/src/main/java/cl/ist/proyectoapp3/AResultado.java
@@ -1,4 +1,14 @@
 package cl.ist.proyectoapp3;
 
-public class AResultado {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultado_main);
+    }
 }
