e7c1e1002a1f883861999f0acca55652687e9331
patriciosoto <72085773+patriciosoto@users.noreply.github.com>
2020-11-05 10:51:37 -0300

    Sexto Commit
    Modificamos el código del AResultado

--- app/src/main/java/cl/ist/proyectoapp3/AResultado.java
+++ app/src/main/java/cl/ist/proyectoapp3/AResultado.java
@@ -1,6 +1,10 @@
 package cl.ist.proyectoapp3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,16 @@ public class AResultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultado_main);
+
+        final Button BtVolver =(Button) findViewById(R.id.btVolver);
+        BtVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sIntent = new Intent(AResultado.this, MainActivity.class);
+                startActivity(sIntent);
+
+            }
+        });
     }
 }
