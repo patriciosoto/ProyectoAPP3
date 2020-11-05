b6e538bbf692865da9063428e461a6c94660c54a
patriciosoto <72085773+patriciosoto@users.noreply.github.com>
2020-11-05 10:32:49 -0300

    Cuarto commit
    Le agregamos mas codigo y otra Main Activity

---
+++ app/src/main/java/cl/ist/proyectoapp3/AResultado.java
@@ -0,0 +1,4 @@
+package cl.ist.proyectoapp3;
+
+public class AResultado {
+}
--- app/src/main/java/cl/ist/proyectoapp3/MainActivity.java
+++ app/src/main/java/cl/ist/proyectoapp3/MainActivity.java
@@ -2,7 +2,11 @@ package cl.ist.proyectoapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button BtAceptar =(Button) findViewById(R.id.btAceptar);
+        BtAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ETNombre=(EditText) findViewById(R.id.etNombreFoto);
+                String StNombre= ETNombre.getText().toString();
+
+                Intent sIntent = new Intent(MainActivity.this, AResultado.class);
+                sIntent.putExtra("STNombre", StNombre);
+                startActivity(sIntent);
+
+            }
+        });
+
     }
 }
