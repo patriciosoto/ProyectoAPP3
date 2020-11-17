dbfea241134023e7fda194e01102bcbd40e8a0ad
patriciosoto <72085773+patriciosoto@users.noreply.github.com>
2020-11-17 08:27:18 -0300

    Octavo commit
    Le agregamos un codigo en el main activity respecto de la actividad 2.2

--- app/src/main/java/cl/ist/proyectoapp3/MainActivity.java
+++ app/src/main/java/cl/ist/proyectoapp3/MainActivity.java
@@ -3,6 +3,8 @@ package cl.ist.proyectoapp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,17 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button BtAceptar =(Button) findViewById(R.id.btAceptar);
+        final Button BtAceptar = (Button) findViewById(R.id.btAceptar);
         BtAceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText ETNombre=(EditText) findViewById(R.id.etNombreFoto);
-                String StNombre= ETNombre.getText().toString();
+                EditText ETNombre = (EditText) findViewById(R.id.etNombreFoto);
+                String StNombre = ETNombre.getText().toString();
+                Image ImageView = (Image) findViewById(R.id.imusuario);
 
                 Intent sIntent = new Intent(MainActivity.this, AResultado.class);
                 sIntent.putExtra("STNombre", StNombre);
                 startActivity(sIntent);
 
+                new Thread(new Runnable() {
+                    public void run() {
+                        final Bitmap bitmap =
+                                loadImageFromNetwork("http://IP.com/image.png");
+                        mImageView.post(new Runnable() {
+                            public void run() {
+                                mImageView.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }).start();
             }
         });
     }
